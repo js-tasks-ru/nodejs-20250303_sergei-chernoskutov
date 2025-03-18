c85e17e22e778a7eab4e05b54a3e48d78305693c
Sergey Chernoskutov <chernoskytofff@gmail.com>
2025-03-18 21:17:25 +0500

    feat(2-module 1-task): add CRUD for tasks

--- 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
+++ 02-nestjs-basics/01-tasks-controller/tasks/tasks.service.ts
@@ -1,17 +1,46 @@
-import { Injectable, NotFoundException } from "@nestjs/common";
+import { Injectable } from "@nestjs/common";
+import { v4 as uuid } from "uuid";
 import { Task } from "./task.model";
 
 @Injectable()
 export class TasksService {
   private tasks: Task[] = [];
 
-  getAllTasks(): Task[] {}
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
 
-  getTaskById(id: string): Task {}
+  getTaskById(id: string): Task | null {
+    return this.tasks.find((task) => task.id === id);
+  }
 
-  createTask(task: Task): Task {}
+  createTask(task: Task): Task {
+    const newTask: Task = {
+      id: uuid(),
+      ...task,
+    };
+    this.setTasks([...this.tasks, newTask]);
+    return newTask;
+  }
 
-  updateTask(id: string, update: Task): Task {}
+  updateTask(id: string, update: Task): Task {
+    const index = this.tasks.findIndex((task) => task.id === id);
+    const tasks = JSON.parse(JSON.stringify(this.tasks));
+    tasks[index] = { ...tasks[index], ...update };
+    this.setTasks(tasks);
 
-  deleteTask(id: string): Task {}
+    return tasks[index];
+  }
+
+  deleteTask(id: string): Task {
+    const task = this.getTaskById(id);
+    if (task) {
+      this.setTasks(this.tasks.filter((task) => task.id !== id));
+    }
+    return task;
+  }
+
+  private setTasks(tasks: Task[]) {
+    this.tasks = tasks;
+  }
 }
